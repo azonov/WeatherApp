94b9248ab8c66b8c1d8c2bfb89af19583299ce84
Dawa Saturday <dawasaturday@gmail.com>
2016-11-26 18:08:59 +0300

    DayForecastVC prototype

---
+++ WeatherApp/WeatherApp/Controller/DayForecastViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DayForecastViewController.swift
+//  WeatherApp
+//
+//  Created by Andrey Volodin on 26.11.16.
+//  Copyright © 2016 VSU. All rights reserved.
+//
+
+import UIKit
+
+public class DayForecastViewController: UIViewController {
+    
+}

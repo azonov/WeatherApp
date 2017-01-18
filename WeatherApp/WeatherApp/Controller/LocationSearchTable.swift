8d00388b14a5e5b4d4f03cc75f74ee4695cca2e9
Leshchinskaya <mariya@leshchinskaya.local>
2017-01-18 18:36:35 +0300

    update files (CityPickerViewController and LocationSearchTable)

--- WeatherApp/WeatherApp/Controller/LocationSearchTable.swift
+++ WeatherApp/WeatherApp/Controller/LocationSearchTable.swift
@@ -6,4 +6,51 @@
 //  Copyright © 2017 VSU. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MapKit
+
+class LocationSearchTable : UITableViewController {
+    var matchingItems:[MKMapItem] = []
+}
+
+extension LocationSearchTable : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard
+            let searchBarText = searchController.searchBar.text else { return }
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBarText
+        let search = MKLocalSearch(request: request)
+        search.start { response, _ in
+            guard let response = response else {
+                return
+            }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension LocationSearchTable {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = selectedItem.name
+        cell.detailTextLabel?.text = selectedItem.locality
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        //dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
+
+
+
+
